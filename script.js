d459be4a89ade05e2c7d67c43f7b057cd30c64d7
Macnelson9 <michaelofatu@gmail.com>
2025-03-26 02:11:26 +0100

    Add loader and enhance mobile navigation functionality

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const menuBtn = document.getElementById('hamburger');
+const closeBtn = document.getElementById('close');
+const mobileNav = document.querySelector('.mobile-nav');
+const navBarContainer = document.querySelector('.nav-bar-container')
+const loaderOverlay = document.querySelector('.loader-overlay')
+
+document.addEventListener('DOMContentLoaded', function () {
+    loaderOverlay.style.display = 'none';
+
+    if (menuBtn) {
+        menuBtn.addEventListener('click', function () {
+            mobileNav.classList.add('show');
+        })
+    }
+
+    if (closeBtn) {
+        closeBtn.addEventListener('click', function () {
+            mobileNav.classList.remove('show');
+        })
+    }
+})
